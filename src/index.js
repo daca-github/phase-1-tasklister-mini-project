a3c56d47fc51f0e9be501b90608741138afab602
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-04-11 20:21:57 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const taskForm = document.getElementById("create-task-form");
+const taskInput = document.getElementById("new-task-description");
+const taskList = document.getElementById("tasks");
+const priorityDropdown = document.getElementById("priority-dropdown");
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const taskText = taskInput.value;
+
+  const priority = priorityDropdown.value;
+
+  const listItem = document.createElement("li");
+  listItem.textContent = taskText;
+  listItem.style.color = priority;
+  if (priority === 'high') {
+    listItem.style.color = 'red'
+  } else if (priority === 'medium') {
+    listItem.style.color = 'orange'
+  } else if (priority === 'low') {
+    listItem.style.color = 'green'
+  }
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = '&#10005;';
+  deleteBtn.addEventListener('click', (event) => {
+    listItem.remove();
+  });
+
+  taskList.appendChild(listItem);
+
+  taskInput.value = "";
 });
+
+taskList.addEventListener("click", (event) => {
+  const clickedElement = event.target;
+
+  if (clickedElement.tagName === "LI") {
+    clickedElement.remove();
+  }
+});
+
+
